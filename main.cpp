59b043e03f4527787c42af53f710156418fcac4a
siegelwang <siegelwang@tencent.com>
2023-12-27 18:42:14 +0800

    doc: 添加main文件的注释

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 using namespace std;
 jmp_buf jmp_;
+// The jmp_buf type is an array type suitable for storing information to restore a calling environment.
+// jmp_buf是debug用的么？调用的地方已被删除
+
 int test(void* attr) {
     char* text = (char*)attr;
     cout << "task test" << attr << endl;
@@ -16,12 +19,13 @@ int main(int argc, char* argv[]) {
     google::InitGoogleLogging((const char *)argv[0]);
     google::SetLogDestination(google::GLOG_INFO,"./"); 
     cout << "1" << endl;
-    
+    // 启动事件监听
     if (EventManager::Instance().Init("127.0.0.1", 12346) != EC_OK) {
         cout << "init err" << endl;
         return 0;      
     }
     cout << "2" << endl;
+    // 添加任务
     EventManager::Instance().AddTask(1000, test, (void*)"1234");
     //cout << "3" << endl;
     EventManager::Instance().Start();
